1323e59d6eaf91d8312127d13e13f682ffdde3b5
Aricoins <arielgarcia79@gmail.com>
2024-08-14 11:39:30 -0300

    backstep

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,74 +1,121 @@
 'use client'
 
-import { Suspense, useEffect } from "react"
-import CartSideBar from "./CartSideBar"
-import Navbar from "./layout/NavBar"
-import { useDispatch, useSelector } from "react-redux"
-import { hideLoading } from "@/redux/slices/cartSlice"
-import { usePathname } from 'next/navigation'
-import { RootState } from "@/redux/store"
+import { Suspense, useEffect, useState } from "react";
+import CartSideBar from "./CartSideBar";
+import Navbar from "./layout/NavBar";
+import { useDispatch, useSelector } from "react-redux";
+import { hideLoading } from "@/redux/slices/cartSlice";
+import { usePathname } from 'next/navigation';
+import { RootState } from "@/redux/store";
 import ClipLoader from "react-spinners/ClipLoader";
-import Image from "next/image"
-import imagen from  './azul.webp'
-import Aos from 'aos'
+import Aos from 'aos';
 import 'aos/dist/aos.css';
 
 export default function App({children}: {children: React.ReactNode}) {
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
     const pathnames = usePathname();
+    const [hovered, setHovered] = useState<string | null>(null);
 
     useEffect(() => {
+        dispatch(hideLoading());
+    }, [dispatch]);
 
-        dispatch(hideLoading())
-    }, [dispatch])
-
-    
     useEffect(() => {
- Aos.init()
-    }, [])
-    const { cartItems, loading } = useSelector((state: RootState) => state.cart)
+        Aos.init();
+    }, []);
+
+    const { cartItems, loading } = useSelector((state: RootState) => state.cart);
+
+    const handleHover = (text: string) => {
+        setHovered(text);
+    };
+
+    const handleHoverEnd = () => {
+        setHovered(null);
+    };
 
     return (
-        <div>
-          <div
+        <div className="relative">
+            <div
                 className={`${
                     loading
                         ? ''
                         : cartItems.length > 0 &&
-                        (pathnames === '/' || pathnames === '/payment' || pathnames === '/shipping' || pathnames.indexOf('/product') >= 0)
+                          (pathnames === '/' || pathnames === '/payment' || pathnames === '/shipping' || pathnames.indexOf('/product') >= 0)
                         ? 'mr-32'
                         : ''
                 }`}
             >
-                {['/signup', '/signin', '/banned', '/verified'].includes(pathnames) ? null : <Navbar />}
-              
-<div className="relative w-full ">
-  <Image 
-    src={imagen}
-    width={1400}
-    height={1400}
-    className="w-full"
-    alt="lagoIA"
-  />
-  <div  data-aos="zoom-in" className="absolute top-20 left-20 text-white bg-black  bg-opacity-25 p-8 text-sm lg:text-7xl z-10 text-center">
-    Bienvenidos a Tienda Azul Lago  
-    <p data-aos="zoom-in" className="text-2xl my-8 text-violet-900">Cosmética, medicinal y aromática natural </p>
-  </div>
-</div>
-    
-                <Suspense fallback={
-                    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
-                        <ClipLoader color="blue" size={150} aria-label="Loading Spinner" data-testid="loader" />
+                {!['/signup', '/signin', '/banned', '/verified'].includes(pathnames) && (
+                    <Navbar  />
+                )}
+
+                <div className="relative w-full mt-32 h-screen overflow-hidden">
+                    <video src='/img/video.mp4' autoPlay loop muted className="absolute inset-0 w-full h-full object-cover" />
+
+                    <div className="absolute inset-0 flex flex-col justify-center items-center text-white p-8">
+                        <h1 
+                            className="text-5xl font-serif tracking-wide transition-transform duration-500 transform hover:scale-110"
+                            onMouseEnter={() => handleHover('Azul Lago')}
+                            onMouseLeave={handleHoverEnd}
+                            data-aos="zoom-in"
+                        >
+                            {hovered === 'Azul Lago' ? 'Bienvenido a' : 'Azul Lago'}
+                        </h1>
+                        <p 
+                            className="text-3xl my-4 text-violet-200 transition-transform duration-500 transform hover:translate-x-2"
+                            onMouseEnter={() => handleHover('Cooperativa')}
+                            onMouseLeave={handleHoverEnd}
+                            data-aos="fade-in-up"
+                        >
+                            {hovered === 'Cooperativa' ? 'Bienvenido a la' : 'Cooperativa de productores de Lago Puelo'}
+                        </p>
+                        <div className="flex flex-col items-center space-y-4 mt-6">
+                            {['Tienda', 'Aceites esenciales', 'Hidrolatos', 'Fitocosmética'].map((item) => (
+                                <p 
+                                    key={item}
+                                    className="text-2xl text-violet-300 transition-transform duration-500 transform hover:scale-105 hover:text-violet-100"
+                                    onMouseEnter={() => handleHover(item)}
+                                    onMouseLeave={handleHoverEnd}
+                                    data-aos="fade-in-up"
+                                >
+                                    {hovered === item ? `Explora nuestros ${item.toLowerCase()}` : item}
+                                </p>
+                            ))}
+                        </div>
                     </div>
-                }>
-                    <main className="p-4">{children}</main>
+                </div>
+
+                <Suspense
+                    fallback={
+                        <div
+                            style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                height: "100vh",
+                                marginTop: "10%"
+                            }}
+                        >
+                            <ClipLoader
+                                color="blue"
+                                size={150}
+                                aria-label="Loading Spinner"
+                                data-testid="loader"
+                            />
+                        </div>
+                    }
+                >
+                    <main className="p-4 pt-40 mt-20 transition-all duration-300">{children}</main>
                 </Suspense>
             </div>
-       
-            <CartSideBar />
 
-
-
+            <CartSideBar
+                // className="transition-transform transform-gpu ease-in-out duration-500"
+                // style={{
+                //     transform: cartItems.length > 0 ? "translateX(0)" : "translateX(100%)",
+                // }}
+            />
         </div>
-    )
+    );
 }
