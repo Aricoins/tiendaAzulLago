87e2ac4a325c4381b1d8370eba6db81aaf8d1110
Aricoins <arielgarcia79@gmail.com>
2024-08-15 04:23:45 -0300

    dashboard

--- src/app/admindashboard/page.tsx
+++ src/app/admindashboard/page.tsx
@@ -89,17 +89,17 @@ useEffect(() => {
   Aos.init({ duration: 1000 });
 })
   console.log(user, "user")
-
+const autorizado = "user_2kfE2AXNYNnmocqzDpFIbegCnRT"
   return (
     !loading ? <h1 data-aos="fade-out">Loading...</h1> : 
   <div>
-        {user ? 
+        {user?.user?.id === autorizado ? 
          <div className=" md:flex md:flex-row w-full place-content-evenly">
 
         <div data-aos="fade-up"  data-aos-duration="1000" className="rounded-lg h-auto md:h-28 md:mt-64 bg-slate-100 bg-opacity-70 shadow-[0_0px_10px_5px_rgba(100,100,100,.8)] m-1 px-2">
           <Link href={'/form'}>
           <button className="bg-blue-500 mx-24 my-10 rounded-full p-4 self-center md:m-7 hover:bg-blue-800 ">
-            Add New Product
+            Agregar Producto
           </button>
           </Link>
         </div>
