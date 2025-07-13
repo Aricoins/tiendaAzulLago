6675c499f247213ab08306a34aa814536ac07b7b
aricoinss <arielrogeldev@gmail.com>
2025-07-13 05:05:22 -0300

    commerce

--- src/app/admindashboard/page.tsx
+++ src/app/admindashboard/page.tsx
@@ -400,7 +400,7 @@ function AdminDashboardContent() {
                 <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600"></div>
             </div>
         );
-    }}
+    }
     return (
         <div className="min-h-screen bg-gray-50">
             {/* Toasts */}
